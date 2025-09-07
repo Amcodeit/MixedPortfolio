37e3c3409b816fcd092871c66f38d753ed612b34
Amit Kumar <162499624+Amcodeit@users.noreply.github.com>
2025-09-07 12:30:02 +0000

    added photo

--- Portfolio/src/components/Intro.jsx
+++ Portfolio/src/components/Intro.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import photo from "../assets/photo.png"
 
 export default function Intro() {
   return (
-    <div className='intro'>
+    <div className="info">
+      <div className="intro">
         <h1>Kirtan Kumar</h1>
-        <h3>And I'm a Frontend Developer</h3>
-        <p>I'm professional web developer with strong skill in HTML, CSS, JavaScript, Tailwind, JQuery etc. I have working in this field almost 3 years and all projects are complited successfully with 100% client satisfiction</p>
-
-        
+        <h3>
+          And I'm a <span>Frontend Developer</span>
+        </h3>
+        <p>
+          I'm professional web developer with strong skill in HTML, CSS,
+          JavaScript, Tailwind, JQuery etc. I have working in this field almost
+          3 years and all projects are complited successfully with 100% client
+          satisfiction
+        </p>
+      </div>
+      <img src={photo} alt="photo" />
     </div>
-  )
+  );
 }
